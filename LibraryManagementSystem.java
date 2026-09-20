f37cb6a0f8f306f99aeb983cf387a4d9b2e98ccf
Shubham Kumar <shubhamkr2020@gmail.com>
2026-09-20 18:49:24 +0530

    Create LibraryManagementSystem.java

---
+++ LibraryManagementSystem.java
@@ -0,0 +1,23 @@
+/*
+
+Library Management System:
+
+1. Clarify the requirements
+2. Create all the classes and the UML diagrams quickly
+3. Check singleton design pattern and strategy patterns if we can use it in the implementation
+4. Implement using java
+
+
+Evaluations on the 5 things:
+
+1. Did you ask c=good clarifying questions to scope out the requirements?
+2. Did you break the problem into classes with correct relationships?
+3. Did you explained the UML and the LLD to the interviewer priperly using the UML diagram?
+4. Did the design followed good design principles?
+5. Did you communicate all your decisions?
+
+Implementation: */
+
+
+
+
