26d5d69d421dec019bfd7ddc359777a46c0da78f
Shubham Kumar <shubhamkr2020@gmail.com>
2026-09-20 18:34:43 +0530

    Create StatePattern.java

---
+++ patterns/StatePattern.java
@@ -0,0 +1,12 @@
+/*
+
+Problem:
+  1. Handle the same inputs/methods when the inputs can be performed in different states of a machine.
+  2. examples:
+    a. Vending machine
+    b. ATM machine
+    c. Doc/Pdf Editor
+
+Implementation: */
+
+
